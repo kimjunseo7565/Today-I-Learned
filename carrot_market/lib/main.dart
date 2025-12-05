565deaf6edff0649f5db670b4aa799abf103beb0
kimjunseo7565 <rlawnstj7565@gmail.com>
2025-12-05 23:32:37 +0900

    Add files via upload

---
+++ carrot_market/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'page/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: App(),
+    );
+  }
+} 
---
+++ carrot_market/lib/page/app.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+import 'home.dart';
+
+class App extends StatefulWidget {
+  App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  int? _curruntPageIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _curruntPageIndex = 0;
+  }
+
+  Widget _bodyWidget() {
+    switch (_curruntPageIndex) {
+      case 0:
+        return Home();
+        break;
+      case 1:
+        return Container();
+        break;
+      case 2:
+        return Container();
+        break;
+      case 3:
+        return Container();
+        break;
+      case 4:
+        return Container();
+        break;
+    }
+    return Container();
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarItem(String iconName, String label) {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: SvgPicture.asset("assets/svg/${iconName}_off.svg", width: 22),
+      ),
+      activeIcon: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: SvgPicture.asset("assets/svg/${iconName}_on.svg", width: 22),
+      ),
+      label: label,
+    );
+  }
+
+  Widget _bottomNavigationBarwidget() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) {
+        setState(() {
+          _curruntPageIndex = index;
+        });
+      },
+      currentIndex: _curruntPageIndex!,
+      selectedItemColor: Colors.black,
+      selectedFontSize: 12,
+      items: [
+        _bottomNavigationBarItem("home", "홈"),
+        _bottomNavigationBarItem("notes", "동네생활"),
+        _bottomNavigationBarItem("location", "내 근처"),
+        _bottomNavigationBarItem("chat", "채팅"),
+        _bottomNavigationBarItem("user", "나의 당근"),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _bodyWidget(),
+      bottomNavigationBar: _bottomNavigationBarwidget(),
+    );
+  }
+}
