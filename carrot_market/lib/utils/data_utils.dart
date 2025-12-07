a2833c8410cf6623c409c9d8ecf99beb95191135
kimjunseo7565 <rlawnstj7565@gmail.com>
2025-12-08 02:07:42 +0900

    Delete carrot_market directory

--- carrot_market/lib/utils/data_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DataUtils {
-  static final oCcy = new NumberFormat("#,###", "ko_KR");
-  static String calcStringToWon(String priceString) {
-    if (priceString == "무료나눔") return priceString;
-    return "${oCcy.format(int.parse(priceString))}원";
-  }
-}
